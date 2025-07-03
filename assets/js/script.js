049cddce8405d1869a0e4f493cc0a6bba7c34fcc
abdullah-ald <abdullah.ahmad.aldali@gmail.com>
2025-07-03 23:32:17 +0300

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
-function switchMood() {
-    const body = document.body;
-    const icon = document.getElementById('dartToLight');
-    
-    body.classList.toggle('dark');
-    if (body.classList.contains('dark')) {
-        icon.classList.remove('fa-moon');
-        icon.classList.add('fa-sun');
-    } else {
-        icon.classList.remove('fa-sun');
-        icon.classList.add('fa-moon');
+const slides=document.querySelectorAll(".container .slide");
+const dotes=document.querySelector(".dotes");
+for(let i =0 ;i<slides.length-2;i++){
+    dotes.innerHTML+=`<span class="dote" onclick=moveSlid(${i})></span>`
+}
+
+function moveSlid(index){
+
+    for(let i=0;i<slides.length;i++){
+        slides[i].style.transform=`translateX(calc(-${index * 100}% - ${4 * index}vw))`
     }
 }
